b9be9195ba57ea586be744993ec141d79433f256
sesn <sesankar11@gmail.com>
2018-06-15 13:26:24 +0530

    added services for accessing the backend

---
+++ src/_services/user.service.js
@@ -0,0 +1,98 @@
+import { authHeader } from '../_helpers';
+let config = {
+  apiUrl: 'http://localhost:4000'
+};
+
+export const userService = {
+  login,
+  logout,
+  register,
+  getAll,
+  getById,
+  update,
+  delete: _delete
+};
+
+function login(username, password) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({username, password})
+  }
+
+  return fetch(`${config.apiUrl}/users/authenticate`, requestOptions)
+    .then(handleResponse)
+    .then(user => {
+      // login successful if there's a jwt token in the response
+      if(user.token) {
+        // store user details and jwt token in local storage to keep user logged in
+        localStorage.setItem('user', JSON.stringify(user));
+      }
+      return user;
+    });
+}
+
+function logout() {
+  // remove user from local storage to log user out
+  localStorage.removeItem('user');
+}
+
+function getAll() {
+  const requestOptions = {
+    method: 'GET',
+    headers: authHeader()
+  };
+
+  return fetch(`${config.apiUrl}/users`, requestOptions).then(handleResponse);
+}
+
+function getById(id) {
+  const requestOptions = {
+    method: 'GET',
+    headers: authHeader()
+  }
+  return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+}
+
+function register(user) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {...authHeader(), 'Content-Type': 'application/json'},
+    body: JSON.stringify(user)
+  };
+  return fetch(`${config.apiUrl}/users/register`, requestOptions).then(handleResponse);
+}
+
+function update(user) {
+  const requestOptions = {
+    method: 'PUT',
+    headers: {...authHeader(), 'Content-Type': 'application/json'},
+    body: JSON.stringify(user)
+  };
+  return fetch(`${config.apiUrl}/users/${user.id}`, requestOptions).then(handleResponse);
+}
+
+function _delete(id) {
+  const requestOptions = {
+    method: 'DELETE',
+    headers: authHeader()
+  };
+  return fetch(`${config.apiUrl}/users/${user.id}`, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+  return response.json().then(data => {
+    if(!response.ok) {
+      if(response.status === 401) {
+        // auto logout if 401 response returned from api
+        logout();
+        location.reload(true);
+      }
+
+      const error = (data && data.error) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
+  })
+}
