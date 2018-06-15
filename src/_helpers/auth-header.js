9da251d02bc4f7327aad21441b26d84435ab9a60
sesn <sesankar11@gmail.com>
2018-06-15 12:44:02 +0530

    fix function declaration error

--- src/_helpers/auth-header.js
+++ src/_helpers/auth-header.js
@@ -1,4 +1,4 @@
-export const authHeader() {
+export function authHeader() {
   // return authorization header with jwt token
   let user = JSON.parse(localStorage.getItem('user'));
   return (user && user.token) ? { 'Authorization': `Bearer ${user.token}` } : {}
